69d2bab63cf08d35e73e687de473a589bd73227a
Robert Sand <robert@pepzi.org>
2024-03-10 19:33:51 +0100

    Ep 3

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
+mod asset_loader;
+mod asteroids;
 mod camera;
+mod collision_detection;
 mod debug;
+mod despawn;
 mod movement;
 mod spaceship;
 
+use asset_loader::AssetLoaderPlugin;
+use asteroids::AsteroidPlugin;
 use bevy::prelude::*;
 use camera::CameraPlugin;
+use collision_detection::CollisionDetectionPlugin;
 use debug::DebugPlugin;
+use despawn::DespawnPlugin;
 use movement::MovementPlugin;
 use spaceship::SpaceshipPlugin;
 
@@ -19,9 +27,13 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         // User configured plugins.
-        .add_plugins(SpaceshipPlugin)
-        .add_plugins(CameraPlugin)
+        .add_plugins(AssetLoaderPlugin)
         .add_plugins(MovementPlugin)
-        .add_plugins(DebugPlugin)
+        .add_plugins(SpaceshipPlugin)
+        .add_plugins(AsteroidPlugin)
+        .add_plugins(CameraPlugin)
+        .add_plugins(CollisionDetectionPlugin)
+        .add_plugins(DespawnPlugin)
+        //.add_plugins(DebugPluginwwwwwwwww)
         .run();
 }
